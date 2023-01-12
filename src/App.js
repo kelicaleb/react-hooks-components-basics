5dfc198f462b75dd3ded542ace715f6af7cbf104
kelicaleb <kelicaleb7@gmail.com>
2023-01-12 17:29:06 +0300

    Completed assignment

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Article from "./Article";
 import Comment from "./Comment";
+import Hello from "./Hello";
 
 function App() {
   return (
     <div>
       <Article />
       <Comment />
+      <Hello />
     </div>
   );
 }
---
+++ src/Hello.js
@@ -0,0 +1,6 @@
+import React from "react"
+
+function Hello(){
+    return( <div>Hello world</div>);
+}
+export default Hello
